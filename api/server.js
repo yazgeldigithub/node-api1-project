fcb246c235f7f20ea0e3968e0ca2a118e22af8ef
yazgeldigithub <devster2016@gmail.com>
2021-07-06 10:07:19 -0500

    5

--- api/server.js
+++ api/server.js
@@ -7,6 +7,37 @@ server.use(express.json())
 
 //Endpoints
 
+server.put('/api/users/:id', async (req, res) => {
+    try{
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else{
+            if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            const updatedUser = await User.update(
+                req.params.id,
+                req.body
+            )
+            res.status(200).json(updatedUser)
+        }
+    }
+}
+    catch(err){
+        res.status(500).json({
+            message: 'error updating users',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+
+})
+
 server.delete('/api/users/:id', async (req, res) => {
    try{
     const possibleUser = await User.findById(req.params.id)
@@ -20,7 +51,7 @@ server.delete('/api/users/:id', async (req, res) => {
     }
    } catch(err) {
     res.status(500).json({
-        message: 'error creating users',
+        message: 'error deleting users',
         err: err.message,
         stack: err.stack,
     })
