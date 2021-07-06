fe5202b8e31582733ce25bdb2d8aa43322f73aff
yazgeldigithub <devster2016@gmail.com>
2021-07-06 09:23:25 -0500

    commit1

--- api/server.js
+++ api/server.js
@@ -1,3 +1,41 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
