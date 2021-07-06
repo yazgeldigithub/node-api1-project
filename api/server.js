aa94a0045a985b27e1395dda195bb871cf7c8dfd
yazgeldigithub <devster2016@gmail.com>
2021-07-06 09:26:55 -0500

    commit2

--- api/server.js
+++ api/server.js
@@ -20,6 +20,11 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
     .then(user => {
+        if(!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
         res.json(user)
     })
     .catch(err => {
