201c8c461692da5273aa11a4a5d004044446bc37
yazgeldigithub <devster2016@gmail.com>
2021-07-06 09:37:40 -0500

    3

--- api/server.js
+++ api/server.js
@@ -1,7 +1,30 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error creating users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
